50e29e823b5f5e2c97414e0b46adaacfbd86d36a
Carrey Lau <nqliujiangtao@gmail.com>
2013-01-13 03:36:41 -0800

    Type commit message here…

--- lib.js
+++ lib.js
@@ -1,10 +1,10 @@
 /**
- *                                                        ____   _____
+ *                                                        ____________
  *  Struts2 Validation -- lib.js, version 1.1             \  /_  /   /
  *  Built at 2012/12/16 08:44 GMT                          \  / /   /
  *                                                          \/ /_  /
  *  (c) baidu.com 2012                                       \  / /
- *  All rights reserved.                                       / /
+ *  All rights reserved.                                      \/ /
  *  Visit https://github.com/mycoin/validation for details     \/
  *
  */
@@ -16,63 +16,72 @@ var debug = function(error, type) {
  * If you would like an application-wide config, change this default name.
  * Otherwise, use the Validate to handle form specific messages.
  */
-;(function(window, document, undefined) {
-    var ENV_DEVELOPMENT = true;
-
+;
+(function(window, document, undefined) {
     var exports = null,
-        baidu = null,
         __closure = {
             version: "dev-1.0.0",
-            validate: {}
+            main: {}
         },
         __utils = {};
 
-    var isset = function(field) {
-            return !(undefined === field);
-        };
-    var trim = function(string) {
-            string = string == null ? "" : string;
-            return string.replace(new RegExp("(^[\\s\\t\\xa0\\u3000]+)|([\\u3000\\xa0\\s\\t]+$)", "g"), "");
-        };
-    var stringify = function(value) {
-            return value == null || value == undefined ? "" : value;
-        };
-    var empty = function(value) {
-            return value == "" || value == null || value == undefined || value == false;
-        };
+    function isset(field) {
+        return !(undefined === field);
+    };
+
+    function trim(string) {
+        string = string == null ? "" : string;
+        return String(string).replace(new RegExp("(^[\\s\\t\\xa0\\u3000]+)|([\\u3000\\xa0\\s\\t]+$)", "g"), "");
+    };
+
+    
+
+    function empty(value) {
+        if(value == null || value == undefined) {
+            value = "";
+        }
+        return value.length == 0;
+    };
     /*
      * Define the regular expressions that will be used
      */
-    __closure.validate = {
+    __closure.main = function() {
 
-        ValidateRequired: function(rule, value) {
-            value = stringify(value);
-            isset(rule["trim"]) && (value = trim(value));
-            return empty(value) ? rule.message : 0;
-        },
 
-        ValidateString: function(rule, value) {
-            var regex = /^\-?[0-9]+$/,
-                result = 0,
+        function stringify(value) {
+            return value == null || value == undefined ? "" : value;
+        };
+
+        return {
+            "required": function(R, value) {
                 value = stringify(value);
+                R["trim"] && (value = trim(value));
+                return empty(value) ? R.message : 0;
+            },
 
-            if(isset(rule["min"]) && value.length < rule["min"] || isset(rule["max"]) && value.length > rule["max"]) {
-                result = rule.message;
+            "string": function(R, value) {
+                var regex = /^\-?[0-9]+$/,
+                    result = 0,
+                    value = stringify(value);
+
+                if(isset(R["min"]) && value.length < R["min"] || isset(R["max"]) && value.length > R["max"]) {
+                    result = R.message;
+                }
+                return empty(value) ? 0 : result;
+            },
+
+            "int": function(R, value) {
+                var regex = /^\-?[0-9]+$/,
+                    result = 0,
+                    value = Number(value);
+
+                if(!/^\-?[0-9]+$/.test(value) || isset(R["min"]) && value < R["min"] || isset(R["max"]) && value > R["max"]) {
+                    result = R.message;
+                }
+                return empty(value) ? 0 : result;
             }
-            return empty(value) ? 0 : result;
-        },
-
-        ValidateInt: function(rule, value) {
-            var regex = /^\-?[0-9]+$/,
-                result = 0,
-                value = Number(value);
-
-            if(!/^\-?[0-9]+$/.test(value) || isset(rule["min"]) && value < rule["min"] || isset(rule["max"]) && value > rule["max"]) {
-                result = rule.message;
-            }
-            return empty(value) ? 0 : result;
         }
-    };
+    }();
     var ruleRegex = /^(.+)\[(.+)\]$/,
         numericRegex = /^[0-9]+$/,
         integerRegex = /^\-?[0-9]+$/,
@@ -85,11 +94,6 @@ var debug = function(error, type) {
         naturalNoZeroRegex = /^[1-9][0-9]*$/i,
         ipRegex = /^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})$/i,
         base64Regex = /[^a-zA-Z0-9\/\+=]/i;
-    /**
-     * 清理不可见多于字符
-     * @param {string} string 事件类型
-     * @return {string} string
-     */
 
     //exception function
     __closure.exception = function(message, type) {
@@ -99,14 +103,24 @@ var debug = function(error, type) {
             return this.type + ": " + this.message;
         }
     };
+    //filter value
+    __closure.trim = function (ele) {
+        var regex = new RegExp("(^[\\s\\t\\xa0\\u3000]+)|([\\u3000\\xa0\\s\\t]+$)", "g")
+        ele.value = ele.value.replace(regex, "");
+    }
+
+    //xss value
+    __closure.xss = function (ele) {
+        ele.value = ele.value.replace(/<\/?[^>]+>/g,''); //remove html tags
+    }
     /**
-     * 序列化表单，转化为JSON格式
+     * Serialization forms, into JSON format
      * @public
      *
      * @param {Element | String} form main one of form elements
      * @param {Object} opt other options
      * @param {Element | String} context handle of validation
-     * @return {Object} 表单数据
+     * @return {Object} Form data
      */
     __closure.serialize = function(form, opt) {
         var eles = form.elements,
@@ -115,61 +129,59 @@ var debug = function(error, type) {
             if(!ele.name || ele.name.substring(0, 1) === ":") {
                 continue;
             }
-            opt.trim == true && (ele.value = __utils.trim(ele.value)); //trim hook
-            if(ele.tagName.toLowerCase() === 'input') {
+            if(ele.tagName.toLowerCase() === "input") {
                 var type = ele.type.toLowerCase();
+                true == opt.without_trim || __closure.trim(ele);
                 switch(type) {
-                case '':
-                case 'text':
-                case 'password':
-                case 'hidden':
+                case "":
+                case "text":
+                case "password":
                     dataMap[ele.name] = ele.value;
                     break;
-                case 'radio':
+                case "radio":
                     (undefined === dataMap[ele.name]) && (dataMap[ele.name] = "");
                     (true === ele.checked) && (dataMap[ele.name] = ele.value);
                     break;
-                case 'checkbox':
+                case "checkbox":
                     (undefined === dataMap[ele.name]) && (dataMap[ele.name] = []);
                     (true === ele.checked) && (dataMap[ele.name].push(ele.value));
                     break;
+                case "hidden":
+                    dataMap[ele.name] = ele.value;
+                    break;
                 }
-            } else if(ele.tagName.toLowerCase() === 'select') {
+            } else if(ele.tagName.toLowerCase() === "select") {
                 dataMap[ele.name] = ele.value;
             } else if(tag === "textarea") {
+                opt.without_xss || __closure.xss(ele); //xss hook
                 dataMap[key] = ele.value;
             }
         }
-        opt.onBeforeValidate && opt.onBeforeValidate.call(null, this, dataMap);
+        typeof opt.onBeforeValidate == "function" && opt.onBeforeValidate(dataMap);
         return dataMap;
     };
     /**
-     * 私有函数，验证单个Ele值正确
+     * Private function, verify the correct individual Ele value
      * @privacy
      *
-     * @param {Object} context 验证规则对象
-     * @param {String} key 表单域的名称
-     * @param {Object} opt 配置项
-     * @return {Array || Number} 验证结果集合
+     * @param {Object} context Validation rule object
+     * @param {String} key the form elements name
+     * @param {Object} opt configuire
+     * @return {Array || Number} result data
      */
     __closure.check = function(context, key, opt) {
         var message = null;
-        var ruleMap = context.rules[key];
+        var ruleMap = context._rulesMap[key];
         var dataMap = context.data;
 
         var result, value = dataMap[key] || null;
-        //resolve errors
-        var resolve = function(errorMsg) {
-                (errorMsg || message === null) && (message = errorMsg);
-            };
         //traverse rules
         for(var type in ruleMap || {}) {
             var rule = ruleMap[type] || {};
-            (type = type.toLowerCase()) && (type = type.substr(0, 1).toUpperCase() + type.substr(1));
-
-            var caller = __closure.validate["Validate" + type] || context.custom["Custom" + type];
+            var caller = __closure.main[type] || context.custom[type];
             if(typeof caller == "function") {
-                resolve(caller.call(null, rule, value, dataMap));
+                var errorMsg = caller.call(null, rule, value, dataMap);
+                (errorMsg || message === null) && (message = errorMsg);
             } else {
                 __closure.warn("Validation function " + type + " is not found, please register it.");
             }
@@ -185,6 +197,12 @@ var debug = function(error, type) {
     __closure.warn = function(message) {
         console.warn(message);
     }
+
+    __closure.exception = function (context, field, message) {
+        this.field = field;
+        this.message = message;
+        return this;
+    };
     /*
      * The exposed public object to validate a form:
      *
@@ -192,13 +210,11 @@ var debug = function(error, type) {
      * @param {Element} rules rule map, key as rule
      * @param {Object} opt [{
      *     trim: true will trim the dom elements
-     *     onBeforeValidate: hook function before validation
-     *     onBeforeValidate: hook function before validation
      *     rules: required|matches[password_confirm]
      * }]
      * @param callback - Function - The callback after validation has been performed.
      */
-    exports = function(ele, rules, opt) {
+    exports = function(ele, rulesMap, opt) {
         if(!(ele && ele.nodeName && ele.nodeType == 1)) {
             ele = document.forms[ele] || document.getElementById(ele);
         }
@@ -209,15 +225,15 @@ var debug = function(error, type) {
             ele = ele.parentNode;
             (ele.tagName == 'BODY') && (ele = null);
         }
-        if(ele) {
-            this.uuid = "$ID" + new Date().getTime();
-            this.rules = rules || {};
+        if(ele && rulesMap) {
+            this._rulesMap = rulesMap || {};
             this.form = ele;
             this.custom = {};
             this.opt = opt || {};
+            this.stack = [];
             return this;
         }
-        throw new __closure.exception("There's no form element to validate.", "ValidateException");
+        throw new __closure.warn("No rules or elements to validate.");
     };
     /**
      * Expose interface function check
@@ -225,39 +241,47 @@ var debug = function(error, type) {
      *
      * @return {NULL | Object} Verification result, return if there is mistake
      */
-    exports.prototype.check = function() {
-        var returns = [],
-            rulesMap = this.rules;
-        this.data = __closure.serialize.call(null, this.form, this.opt) || {};
-
+    exports.prototype.check = function(data) {
+        this.stack = [];
+        var rulesMap = this._rulesMap;
+        this.data = data || __closure.serialize(this.form, this.opt) || {};
         for(var key in rulesMap) {
-            var ret = __closure.check.call(null, this, key);
+            var message = __closure.check(this, key);
+            if(0 == message || undefined == message || null == message) {
+                continue;
+            }
+            var error = new __closure.exception(this, key, message);
             //push error message
-            ret && returns.push(ret);
+            this.stack.push(error);
         }
-        return returns.length > 0 ? returns : true;
+        return this.stack.length == 0;
     };
-    /**
-     * Registered custom validation function, parameters {data, name}
-     * @public
-     *
-     * @return {NULL | Object} Verification result, return if there is mistake
-     */
+
+    exports.prototype.getException = function() {
+        if(this.stack.length == 0) {
+            return null;
+        } else {
+            return this.stack[0];
+        }
+    };
+    //get error message 
+    exports.prototype.getMessage = function() {
+        return this.getException().message;
+    };
+    //get field name
+    exports.prototype.getFieldName = function() {
+        return this.getException().field;
+    };
+    //Registered custom validation function, parameters {data, name}
     exports.prototype.register = function(type, method) {
-        this.custom["Custom" + type] = method;
+        this.custom[type] = method;
+        return this;
     };
-    //check if the arguements illegal
-    exports.prototype.interrupt = function(key, value) {
-        
-
-
-
-
-
-    }
+    //set parameter
+    exports.prototype.setParameter = function(key, value) {
+        this.opt[key] = value;
+        return this;
+    };
     //export it out
     Form.Validate = exports;
-
-
-
 })(window, document);

bfbf1667c32032f92785d5d62090d4bddb75fa72
mycoin <nqliujiangtao@gmail.com>
2014-02-17 16:24:36 +0800

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# author:  mycoin (nqliujiangtao@gmail.com)
-# date:    2014/1/22
-# repos:   https://github.com/mycoin/validation
-
-java -jar tool/webpacker.jar source.js --mode 1 --charset utf-8 -o min.js
-echo "OK."
